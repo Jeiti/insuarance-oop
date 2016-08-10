e6a7729ccd1a0fc8a73b13b7d9479b7d0496deae
Jeiti <jeiti@list.ru>
2016-08-10 09:38:42 +0300

    Разобрались с php-activerecord

---
+++ models/News.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: evgen
+ * Date: 10.08.16
+ * Time: 9:11
+ */
+class News extends ActiveRecord\Model{
+
+}
--- test.php
+++ test.php
@@ -1,27 +1,40 @@
 <?php
-//включить отображение ошибок
-error_reporting(E_ALL);
+
+define("ERROR_LEVEL", E_ALL|E_STRICT);
+ini_set("error_reporting",E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-//конец включить отображение ошибок
 
-//подключение autoload
-require ("config.inc");
-//конец подключение autoload
+require_once('vendor/php-activerecord/php-activerecord/ActiveRecord.php');
+ActiveRecord\Config::initialize(function($cfg){
+    $cfg->set_model_directory('models');
+    $cfg->set_connections(array('development' => 'mysql://root:123@localhost/insuarance'));
+});
 
-$controller = new TestController(); //создается новый экземпляр класса TestController() в котором
-//подключается TestView() и TestModel() автоматически
+/*# create Tito
+$user = User::create(array('name' => 'Tito', 'state' => 'VA'));
 
-//реализация маршрутизации
-$action=(isset($_GET['action']))?$_GET['action']:"index";//получаем - какую страницу выбрал пользователь
+# read Tito
+$user = User::find_by_name('Tito');
 
-switch ($action){
-    case "index":
-        $controller->actionIndex();
-        break;
-/*    case "new":
-        $controller->actionNew();
-        break;
-    default:
-        $controller->actionIndex();*/
-}
+# update Tito
+$user->name = 'Tito Jr';
+$user->save();
+
+# delete Tito
+$user->delete();*/
+
+
+
+# create Tito
+//$news = News::create(["title"=>"news", "content"=>"123"]);
+$news = News::find(35);
+echo $news->title;
+
+//TODO: потренироваться с CRUD для новостей
+//TODO: подключить php-activerecord к нашему framework
+
+//TODO: front-controller нужен для работы с контроллерами
+//TODO: smarty - нужен для работы с view
+//TODO: php-activerecord нужен для работы с models
+//TODO: если самому писать mvc-framework то нужно использовать шаблоны проектирования
